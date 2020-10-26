fb0d3c81019f35f06074504f617260380544524d
Matt Callahan <matthew.callahan@outlook.com>
2020-10-26 07:19:55 -0700

    import BrowserModules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from 'src/app/app.component';
 import { BoardComponent } from 'src/app/components/board/board.component';
 
 @NgModule({
   declarations: [AppComponent, BoardComponent],
-  imports: [],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
